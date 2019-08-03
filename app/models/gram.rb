9b331baa455748260ce7165fc14f1cb8eb7f74d8
Toliusis34 <matttoliusis1@gmail.com>
2019-08-03 07:54:41 +0000

    styled application

--- app/models/gram.rb
+++ app/models/gram.rb
@@ -1,5 +1,7 @@
 class Gram < ApplicationRecord
-	mount_uploader :picture, PictureUploader
 	validates :message, presence: true
+	validates :picture, presence: true
+	has_many :comments
+	mount_uploader :picture, PictureUploader
 	belongs_to :user
 end
